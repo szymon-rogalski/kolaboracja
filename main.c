93e291687993fea4cf10671107c92e230d8ba88b
KonradKulig <255946@edu.p.lodz.pl>
2025-06-11 21:44:55 +0200

    dodanie funkcji zapis

--- main.c
+++ main.c
@@ -23,6 +23,7 @@ void menu(void){
     printf("4. Wyswietl najwieksza wartosc z tablicy. \n");
     printf("5. Wyswietl srednia wartosc liczb z tablicy.\n");
     printf("6. Zapis do pliku.\n");
+    printf("7.Wczytaj z pliku\n");
     printf("0. Wyjdz z programu.\n");
 }
 void numbers(int array[SIZE], int size){
@@ -89,64 +90,84 @@ float average(int *array){
 FILE *fptr;
 int zapis(int *array){
 
-    fptr = fopen("zapisy.txt", "w");
-    if (fptr == 0){
-        printf("blad");
-        exit(1);
+        fptr = fopen("zapisy.txt", "w");
+        if (fptr ==0){
+            return 1;
+        }
+        for (int i=0; i < SIZE; i++){
+            fprintf(fptr,"\n %d \n", array[i]);
+        }
+        fclose(fptr);
+        return 0;
     }
-    int i;
-    for(i=0;i<SIZE;i++){
-        fprintf(fptr,"%d",array[i]);
+int load(int *array) {
+    FILE *fptr;
+    fptr = fopen("zapisy.txt", "r");
+    if (fptr == NULL) {
+        printf("Brak pliku.\n\n");
+        return 1;
+    }
+    int i = 0;
+    while (fscanf(fptr, "%d", &array[i]) == 1) {
+        i++;
+        if (i >= SIZE)
+            break;
     }
     fclose(fptr);
+    printf("Wczytano tablice z pliku.\n\n");
     return 0;
 }
 
 int main() {
-    int array [SIZE] = {1,2,3,4,5};
-    int option = 0,erroeCode = 0;
+        int array [SIZE] = {1,2,3,4,5};
+        int option = 0,erroeCode = 0;
 
-    do{
-        menu();
-        scanf("%d",&option);
+        do{
+            menu();
+            scanf("%d",&option);
 
-        switch (option) {
-            case 0:
-                break;
-            case 1:
-                printf("Wprowadzam dane do tablicy\n\n");
+            switch (option) {
+                case 0:
+                    break;
+                case 1:
+                    printf("Wprowadzam dane do tablicy\n\n");
                 numbers(array, SIZE);
                 break;
-            case 2:
-                printArray(array);
+                case 2:
+                    printArray(array);
                 break;
-            case 3:
-                printf("okreslam najmniejsza wartosc: %d \n\n", minimum(array));
+                case 3:
+                    printf("okreslam najmniejsza wartosc: %d \n\n", minimum(array));
 
                 break;
-            case 4:
-                printf("najwieksza wartosc to: %d\n\n", maksimum(array));
+                case 4:
+                    printf("najwieksza wartosc to: %d\n\n", maksimum(array));
                 break;
-            case 5:
-                printf("okreslam srednia wartosc liczb: %f \n\n", average(array));
+                case 5:
+                    printf("okreslam srednia wartosc liczb: %f \n\n", average(array));
                 break;
-            case 6:
-                printf("zapisywanie...\n");
+                case 6:
+                    printf("zapisywanie...\n");
                 erroeCode = zapis(array);
                 if(!erroeCode){
                     printf("zapisane\n");
                 }
                 else printf("nie udalo sie\n");
                 break;
-            default:
-                printf("wybierz wlasciwa opcje\n\n");
+                case 7:
+                    erroeCode = load(array);
+                if (!erroeCode)
+                    printf("Dane wczytane poprawnie z pliku.\n");
+                break;
+                default:
+                    printf("wybierz wlasciwa opcje\n\n");
+            }
+
         }
+        while (option != 0);
 
+        printf("koniec programu");
+
+        return 0;
     }
-    while (option != 0);
-
-    printf("koniec programu");
-
-    return 0;
-}
 
